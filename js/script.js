9868c05047d61d984475eba0ac3bce25e7361592
minju9927 <pink5214@naver.com>
2025-03-05 17:04:59 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+gsap.from('#intro .char', {
+    yPercent: 110,
+    stagger: 0.03,
+    duration: .3,
+    ease: 'power1'
+})
+// gsap.from('#intro .char', {
+//     xPercent: -110,
+//     rotationX: 360,
+//     opacity: 0,
+//     stagger: 0.03,
+//     duration: .3,
+//     ease: 'power1'
+// })
+
+
+const koTime = document.querySelector('#koTime')
+
+
+function updateTime() {
+   
+    const koreaTime =new Date().toLocaleTimeString("en-US",{
+        timeZone:'Asia/Seoul',
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit',
+        hour12:false
+    })
+    koTime.textContent=koreaTime
+
+}
+updateTime()
+setInterval(updateTime, 1000)
